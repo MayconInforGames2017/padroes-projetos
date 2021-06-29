711e6b831e8a1bcd041c5a0b308cb03798775a7c
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-06-29 19:19:41 -0300

    criando o pacote nullobject e a classe carrinho

---
+++ carrinho/src/main/java/br/upe/processosoftware/nullobject/Carrinho.java
@@ -0,0 +1,33 @@
+package br.upe.processosoftware.nullobject;
+
+public class Carrinho {
+
+	private String nomeUsuario;
+	private Double valor;
+	private Integer tamanho;
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Integer getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(Integer tamanho) {
+		this.tamanho = tamanho;
+	}
+
+}

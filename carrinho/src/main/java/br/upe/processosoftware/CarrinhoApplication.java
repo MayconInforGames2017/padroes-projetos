c9a62d1c868391d2b7fd9bbbd6b853d0c377839e
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-06-29 18:38:27 -0300

    criando o projeto carrinho

---
+++ carrinho/src/main/java/br/upe/processosoftware/CarrinhoApplication.java
@@ -0,0 +1,13 @@
+package br.upe.processosoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoApplication.class, args);
+	}
+
+}
